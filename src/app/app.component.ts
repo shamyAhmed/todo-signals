4fe9571b10eb96b955a00f1a2e678df948b380cf
shamyAhmed <ahmedelshamybea@gmail.com>
2024-12-08 21:52:24 +0200

    feat: implementing the layout functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RootLayout } from './layouts/rootLayout/RootLayout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, RootLayout],
+  template: `
+    <td-root-layout>
+      <router-outlet></router-outlet>
+    </td-root-layout>
+  `,
 })
 export class AppComponent {
   title = 'todo-signals';
---
+++ src/app/layouts/rootLayout/RootLayout.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "td-root-layout",
+    templateUrl: "RootLayout.html",
+    styleUrl: "RootLayout.css"
+})
+export class RootLayout {
+
+}
